e5045f1eeb8c7567c127c4453a06e840839b16da
[Uzma Syed] <[uzsasyed@gmail.com]>
2022-03-06 22:05:28 -0500

    Updated

---
+++ Python_practice.py
@@ -0,0 +1,29 @@
+
+
+# What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')
+    
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+
+
+
+
+
+
+
